6047586a46b2f01d63873ff379e240ae070f3d52
Yassin <yassinmouallia@gmail.com>
2025-01-16 23:47:18 +0100

    Fix import statement in weather report service

--- src/services/weather-report.service.ts
+++ src/services/weather-report.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
-import {env}
+
 
 @Injectable({
   providedIn: 'root'
